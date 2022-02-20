9f15b86f1f83d4ffd9bc06fa6c6790f0c0a3cc72
Ricardo Arias Salazar <rari0002@student.monash.edu>
2022-02-20 20:33:43 +1100

    Conenect to Heroku Database

--- website/config.py
+++ website/config.py
@@ -2,7 +2,8 @@ import os
 
 class Config:
   SECRET_KEY = os.environ.get('SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+  SQLALCHEMY_DATABASE_URI = "postgres://stvecqbaffkkvl:58c895902c77f86180d0fa4737f15bca11fe64492bd0f2ddcbc5abf36bd566fa@ec2-54-172-219-6.compute-1.amazonaws.com:5432/d5g8u6ucev6dpa"
+#os.environ.get('SQLALCHEMY_DATABASE_URI')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   MAIL_SERVER = 'smtp.googlemail.com'
   MAIL_PORT = 465
